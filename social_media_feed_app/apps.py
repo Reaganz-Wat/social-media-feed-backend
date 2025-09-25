499382b4b26e80baa44759902a70b8c3007d3bcb
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-09-25 07:04:42 +0300

    feat: registered signals in app

--- social_media_feed_app/apps.py
+++ social_media_feed_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SocialMediaFeedAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social_media_feed_app'
+
+    def ready(self):
+        # Import signals to ensure they're registered
+        import social_media_feed_app.signals
